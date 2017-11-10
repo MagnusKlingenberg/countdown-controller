5abc51e0ac146c1dc888b0cac2d85fa78436a649
Magnus Klingenberg <magnus.klingenberg@gmail.com>
2017-11-10 22:40:54 +0000

    Added current and static time outputs

--- display.js
+++ display.js
@@ -7,30 +7,65 @@ function Row(addr_1, addr_2, addr_3, bus) {
   this.disp_3 = new SevenSegment(addr_3, bus);
 }
 
-Row.prototype.writeTime = function(timestamp) {
+Row.prototype.writeTimeDiff = function(timestamp) {
   var negative = 0;
   if (timestamp < 0) {
     timestamp = Math.abs(timestamp);
     negative = 0xff;
   }
   var date = new Date(timestamp * 1000);
-  this.disp_1.writeDigit(4, date.getSeconds() % 10);
-  this.disp_1.writeDigit(3, (date.getSeconds() - (date.getSeconds() % 10)) / 10);
+  var seconds = date.getSeconds();
+  var minutes = date.getMinutes();
+  var hours   = date.getHours();
+  var days    = date.getDate() - 1;
+  var months  = date.getMonth();
+  var years   = date.getFullYear();
 
-  this.disp_1.writeDigit(1, date.getMinutes() % 10);
-  this.disp_1.writeDigit(0, (date.getMinutes() - (date.getMinutes() % 10)) / 10);
+  this.disp_1.writeDigit(4, seconds % 10);
+  this.disp_1.writeDigit(3, (seconds - (seconds % 10)) / 10);
 
-  this.disp_2.writeDigit(4, date.getHours() % 10);
-  this.disp_2.writeDigit(3, (date.getHours() - (date.getHours() % 10)) / 10);
+  this.disp_1.writeDigit(1, minutes % 10);
+  this.disp_1.writeDigit(0, (minutes - (minutes % 10)) / 10);
 
-  this.disp_2.writeDigit(1, date.getDate() % 10);
-  this.disp_2.writeDigit(0, (date.getDate() - (date.getDate() % 10)) / 10);
+  this.disp_2.writeDigit(4, hours % 10);
+  this.disp_2.writeDigit(3, (hours - (hours % 10)) / 10);
 
-  this.disp_3.writeDigit(4, date.getMonth() % 10);
-  this.disp_3.writeDigit(4, (date.getMonth() - (date.getMonth() % 10)) / 10);
+  this.disp_2.writeDigit(1, days % 10);
+  this.disp_2.writeDigit(0, (days - (days % 10)) / 10);
 
-  this.disp_3.writeDigit(1, date.getFullYear() % 10);
+  this.disp_3.writeDigit(4, months % 10);
+  this.disp_3.writeDigit(3, (months - (months % 10)) / 10);
+
+  this.disp_3.writeDigit(1, years % 10);
   this.disp_3.writeDigitRaw(0, negative & 0x40);
+}
+
+Row.prototype.writeTime = function(timestamp) {
+  var date = new Date(timestamp * 1000);
+  var seconds = date.getSeconds();
+  var minutes = date.getMinutes();
+  var hours   = date.getHours();
+  var days    = date.getDate();
+  var months  = date.getMonth() + 1 ;
+  var years   = date.getFullYear();
+
+  this.disp_1.writeDigit(4, seconds % 10);
+  this.disp_1.writeDigit(3, (seconds - (seconds % 10)) / 10);
+
+  this.disp_1.writeDigit(1, minutes % 10);
+  this.disp_1.writeDigit(0, (minutes - (minutes % 10)) / 10);
+
+  this.disp_2.writeDigit(4, hours % 10);
+  this.disp_2.writeDigit(3, (hours - (hours % 10)) / 10);
+
+  this.disp_2.writeDigit(1, days % 10);
+  this.disp_2.writeDigit(0, (days - (days % 10)) / 10);
+
+  this.disp_3.writeDigit(4, months % 10);
+  this.disp_3.writeDigit(3, (months - (months % 10)) / 10);
+
+  this.disp_3.writeDigit(1, years % 10);
+  this.disp_3.writeDigit(0, ((years % 100) - (years %10)) / 10);
 }
 
 module.exports = Row;
--- index.js
+++ index.js
@@ -2,23 +2,26 @@ var Server = require('./server');
 var Row = require('./display');
 var TimerModel = require('./api/models/timerModel');
 
-var row_1 = new Row(0x70, 0x71, 0x72, 1);
-var row_2 = new Row(0x73, 0x74, 0x75, 1);
+var row_0 = new Row(0x70, 0x71, 0x72, 1);
+var row_1 = new Row(0x73, 0x74, 0x75, 1);
 var server = new Server(3000);
 
 
-function timeDiff(config) {
+function displayConfig(config, row) {
   var now = Math.floor(Date.now() / 1000);
   if (config.mode == 'upfrom') {
-    return now - config.startAt;
+    row.writeTimeDiff(now - config.startAt);
   } else if (config.mode == 'downto') {
-    return config.startAt - now;
+    row.writeTimeDiff(config.startAt - now);
+  } else if (config.mode == 'static') {
+    row.writeTime(config.startAt);
   } else {
-    return 0;
+    row.writeTime(now);
   }
 };
 
 var interval = setInterval( function() {
-    row_1.writeTime(timeDiff(TimerModel.timerList[0]));
+    displayConfig(TimerModel.timerList[0], row_0);
+    displayConfig(TimerModel.timerList[1], row_1);
 }, 200);
 
