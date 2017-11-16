7e3588695e77a39ad106f4efdf23695c516d1126
Magnus Klingenberg <magnus.klingenberg@gmail.com>
2017-11-16 22:08:01 +0100

    Updated display address order

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var Server = require('./server');
 var Row = require('./display');
 var TimerModel = require('./api/models/timerModel');
 
-var row_0 = new Row(0x70, 0x71, 0x72, 1);
-var row_1 = new Row(0x73, 0x74, 0x75, 1);
+var row_0 = new Row(0x76, 0x73, 0x75, 1);
+var row_1 = new Row(0x70, 0x71, 0x72, 1);
 var server = new Server(3000);
 
 
